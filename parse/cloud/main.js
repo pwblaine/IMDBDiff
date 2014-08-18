6ea00a94372e12146ffcdf2705a7cff42810ed21
Patrick Blaine <pwblaine@gmail.com>
2014-08-17 23:48:48 -0700

    added parse example

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -8,16 +8,22 @@
  */
 
 Parse.Cloud.define('request', function(request, response) {
-  if (request) {
-    response.success(request);
-  } else {
-    response.error('unable to access the request data');
-  }
-});
+                   
+                   if (request) {
+                   
+                   response.success(request);
+                   
+                   } else {
+                   
+                   response.error('unable to access the request data');
+                   
+                   }
+                   
+                   });
 
 /*
  *  @name Parse.Cloud.run('search',data,options)
- *  
+ *
  *  @params data must be a string and is a movie title to search for
  *
  *  @description 'search' contacts the The OMDb API ( http://www.omdbapi.com ) and requests movies with the title
@@ -26,13 +32,67 @@ Parse.Cloud.define('request', function(request, response) {
  */
 
 Parse.Cloud.define('searchForMoviesWithTitle', function(request, response) {
-  if (request) {
-     response.success(request);
-  } else {
-     response.error('unable to access the request data');
-  }
-});
+                   
+                   if (request) {
+                   
+                   response.success(request);
+                   
+                   } else {
+                   
+                   response.error('unable to access the request data');
+                   
+                   }
+                   
+                   });
 
 Parse.Cloud.job("runSearch", function(request, status) {
+                
                 status.success(Parse.Cloud.searchForMoviesWithTitle("title"));
+                
+                });
+
+Parse.Cloud.job("userMigration", function(request, status) {
+                
+                // Set up to modify user data
+                
+                Parse.Cloud.useMasterKey();
+                
+                var counter = 0;
+                
+                // Query for all users
+                
+                var query = new Parse.Query(Parse.User);
+                
+                query.each(function(user) {
+                
+                           // Update to plan value passed in
+                           
+                           user.set("plan", request.params.plan);
+                           
+                           if (counter % 100 === 0) {
+                           
+                           // Set the  job's progress status
+                           
+                           status.message(counter + " users processed.");
+                           
+                           }
+                           
+                           counter += 1;
+                           
+                           return user.save();
+                           
+                           }).then(function() {
+                           
+                                   // Set the job's success status
+                                   
+                                   status.success("Migration completed successfully.");
+                                   
+                                   }, function(error) {
+                                   
+                                   // Set the job's error status
+                                   
+                                   status.error("Uh oh, something went wrong.");
+                                   
+                                   });
+                
                 });
