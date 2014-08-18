3c6741d4733552ea8353cf1d68b48ccee86c113b
Patrick Blaine <pwblaine@gmail.com>
2014-08-17 23:32:47 -0700

    added in parse job example

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -36,3 +36,27 @@ Parse.Cloud.define('searchForMoviesWithTitle', function(request, response) {
 Parse.Cloud.job("runSearch", function(request, status) {
                 status.success(Parse.Cloud.searchForMoviesWithTitle("title"));
                 });
+
+Parse.Cloud.job("userMigration", function(request, status) {
+                // Set up to modify user data
+                Parse.Cloud.useMasterKey();
+                var counter = 0;
+                // Query for all users
+                var query = new Parse.Query(Parse.User);
+                query.each(function(user) {
+                           // Update to plan value passed in
+                           user.set("plan", request.params.plan);
+                           if (counter % 100 === 0) {
+                           // Set the  job's progress status
+                           status.message(counter + " users processed.");
+                           }
+                           counter += 1;
+                           return user.save();
+                           }).then(function() {
+                                   // Set the job's success status
+                                   status.success("Migration completed successfully.");
+                                   }, function(error) {
+                                   // Set the job's error status
+                                   status.error("Uh oh, something went wrong.");
+                                   });
+                });
