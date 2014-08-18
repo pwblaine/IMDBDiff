77079b7a928036a528d162a19f2c8277ac362337
Patrick Blaine <pwblaine@gmail.com>
2014-08-18 00:41:13 -0700

    fixed runSearch test

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -35,7 +35,7 @@ Parse.Cloud.define('searchForMoviesWithTitle', function(request, response) {
                    
                    if (request) {
                    
-                   response.success(request);
+                   response.success(request.params);
                    
                    } else {
                    
@@ -47,52 +47,6 @@ Parse.Cloud.define('searchForMoviesWithTitle', function(request, response) {
 
 Parse.Cloud.job("runSearch", function(request, status) {
                 
-                status.success(Parse.Cloud.searchForMoviesWithTitle("title"));
-                
-                });
-
-Parse.Cloud.job("userMigration", function(request, status) {
-                
-                // Set up to modify user data
-                
-                Parse.Cloud.useMasterKey();
-                
-                var counter = 0;
-                
-                // Query for all users
-                
-                var query = new Parse.Query(Parse.User);
-                
-                query.each(function(user) {
-                
-                           // Update to plan value passed in
-                           
-                           user.set("plan", request.params.plan);
-                           
-                           if (counter % 100 === 0) {
-                           
-                           // Set the  job's progress status
-                           
-                           status.message(counter + " users processed.");
-                           
-                           }
-                           
-                           counter += 1;
-                           
-                           return user.save();
-                           
-                           }).then(function() {
-                           
-                                   // Set the job's success status
-                                   
-                                   status.success("Migration completed successfully.");
-                                   
-                                   }, function(error) {
-                                   
-                                   // Set the job's error status
-                                   
-                                   status.error("Uh oh, something went wrong.");
-                                   
-                                   });
+                status.success(Parse.Cloud.run('searchForMoviesWithTitle',request.params));
                 
                 });
